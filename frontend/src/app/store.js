8624b165fae587cf6e71ac04d3e4c6859b80b343
fivano7 <filipivanovic7@gmail.com>
2022-12-01 13:07:26 +0100

    register and logout

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import authReducer from '../features/auth/authSlice';
 
+//2.) dodat ga ovdje u reducere, store.js je već postojeći file
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    auth: authReducer,
   },
 });
