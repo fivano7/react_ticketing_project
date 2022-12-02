a68dbbaf32946b1f4c5f6ccb5e92eb78f5f0f242
fivano7 <filipivanovic7@gmail.com>
2022-12-02 10:34:44 +0100

    Gotov projekt

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import ticketReducer from '../features/tickets/ticketSlice';
+import noteReducer from '../features/notes/noteSlice';
 
 //2.) dodat ga ovdje u reducere, store.js je već postojeći file
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     tickets: ticketReducer,
+    notes: noteReducer,
   },
 });
--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,12 +1,38 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
+import Modal from 'react-modal';
+import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
+import {
+  getNotes,
+  createNote,
+  reset as notesReset,
+} from '../features/notes/noteSlice';
 import { useParams, useNavigate } from 'react-router-dom';
+import { FaPlus } from 'react-icons/fa';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
-import { useEffect } from 'react';
+import NoteItem from '../components/NoteItem';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
+//za modal
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+  },
+};
+
+Modal.setAppElement('#root');
+
 function Ticket() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState('');
   const params = useParams();
   const navigate = useNavigate();
 
@@ -15,6 +41,9 @@ function Ticket() {
   const { ticket, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.tickets
   );
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
+  );
 
   useEffect(() => {
     if (isError) {
@@ -22,6 +51,7 @@ function Ticket() {
     }
 
     dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
   }, [isError, message, ticketId]);
 
   const onTicketClose = () => {
@@ -30,7 +60,19 @@ function Ticket() {
     navigate('/tickets'); //navigiramo tako da ne moramo refreshati stranicu manualno
   };
 
-  if (isLoading) {
+  //Create note submit
+  const onNoteSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createNote({ noteText, ticketId }));
+    closeModal();
+  };
+
+  //Open/close modal
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
+  if (isLoading || notesIsLoading) {
     return <Spinner />;
   }
 
@@ -57,13 +99,53 @@ function Ticket() {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
 
       {ticket.status !== 'closed' && (
-        <button
-          className='btn btn-block btn-danger'
-          onClick={onTicketClose}
-        >Close Ticket</button>
+        <button className='btn' onClick={openModal}>
+          <FaPlus />
+          Add Note
+        </button>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel='Add Note'
+      >
+        <h2>Add Note</h2>
+        <button className='btn-close' onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={onNoteSubmit}>
+          <div className='form-group'>
+            <textarea
+              name='noteText'
+              id='noteText'
+              className='form-control'
+              placeholder='Note text'
+              value={noteText}
+              onChange={(e) => setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className='form-group'>
+            <button className='btn' type='submit'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
+
+      {ticket.status !== 'closed' && (
+        <button className='btn btn-block btn-danger' onClick={onTicketClose}>
+          Close Ticket
+        </button>
       )}
     </div>
   );
